4e43e45cc477e4a714a6854302ae66875ef9b5c0
menoo-o <menosuper6@gmail.com>
2024-12-05 19:01:04 +0500

    project pg tweaks

--- app/components/ProjectCard/ProjectCard.tsx
+++ app/components/ProjectCard/ProjectCard.tsx
@@ -1,25 +1,14 @@
-import Image from 'next/image';
+
 import styles from './ProjectCard.module.css';
 
 export default function ProjectCard({ project }) {
-  const { title, description, images, tags, demoLink, repoLink } = project; // Assuming `images` is an array of image URLs.
+  const { title, description,  tags, demoLink, repoLink } = project; // Assuming `images` is an array of image URLs.
 
   return (
     <div className={styles.card}>
       {/* Grid of images */}
       <div className={styles.imageGrid}>
-        {images.slice(0, 3).map((img, index) => (
-          <div key={index} className={styles.imageWrapper}>
-            <Image
-              src={img}
-              alt={`${title} screenshot ${index + 1}`}
-              className={styles.image}
-              width={350} // Adjust width
-              height={300} // Adjust height
-              priority={index === 0} // Prioritize the first image
-            />
-          </div>
-        ))}
+        
       </div>
 
       {/* Project details */}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,92 +4,51 @@ import ProjectCard from '../components/ProjectCard/ProjectCard';
 const projects = [
     {
         id: 1,
-        title: "Project One",
-        description: "A brief description of the project.",
-        images: [
-          "/pr1.3.png",
-          "/pr1.2.png",
-          "/pr1.3.png"
-        ],
-        tags: ["React", "Node.js"],
+        title: "Grains & Essentials",
+        description: "A modern grocery store application built with the MERN stack, offering seamless product browsing, cart management, and checkout for everyday essentials.",
+         tags: ["React", "Node.js", "Express Js", "Canva"],
         demoLink: "https://demo-link.com",
         repoLink: "https://github.com/repo",
       },
 
       {
         id: 2,
-        title: "Project One",
-        description: "A brief description of the project.",
-        images: [
-          "/pr1.3.png",
-          "/pr1.2.png",
-          "/pr1.3.png"
-        ],
-        tags: ["React", "Node.js"],
-        demoLink: "https://demo-link.com",
-        repoLink: "https://github.com/repo",
+        title: "Chess-Drag & Drop with JS",
+        description: "A dynamic chess game implemented in JavaScript, showcasing intuitive drag-and-drop functionality for moving pieces seamlessly across the board.",
+        tags: ["HTML", "CSS", "JS"],
+        demoLink: "https://hilarious-cranachan-7e9369.netlify.app/",
+        repoLink: "https://github.com/menoo-o/chess-game",
       },
 
       {
         id: 3,
-        title: "Project One",
-        description: "A brief description of the project.",
-        images: [
-          "/pr1.3.png",
-          "/pr1.2.png",
-          "/pr1.3.png"
-        ],
+        title: "Weather API",
+        description: "A web app that fetches and displays real-time weather data for random cities by interacting with a weather API, offering a simple and dynamic way to explore forecasts.",
         tags: ["React", "Node.js"],
-        demoLink: "https://demo-link.com",
-        repoLink: "https://github.com/repo",
+        demoLink: "https://vocal-sorbet-783909.netlify.app/",
+        repoLink: "https://github.com/menoo-o/Weather-API",
       },
 
       {
         id: 4,
-        title: "Project One",
-        description: "A brief description of the project.",
-        images: [
-          "/pr1.3.png",
-          "/pr1.2.png",
-          "/pr1.3.png"
-        ],
-        tags: ["React", "Node.js"],
-        demoLink: "https://demo-link.com",
-        repoLink: "https://github.com/repo",
+        title: "Solar Flare Warning",
+        description: "A visually captivating landing page built with Three.js, utilizing SphereGeometry to create an immersive 3D illusion of a solar flare, blending design and interactivity",
+        tags: ["HTML", "CSS", "three.js"],
+        demoLink: "https://aaagh.netlify.app/",
+        repoLink: "https://github.com/menoo-o/solar-flare",
       },
 
-      {
-        id: 5,
-        title: "Project One",
-        description: "A brief description of the project.",
-        images: [
-          "/pr1.3.png",
-          "/pr1.2.png",
-          "/pr1.3.png"
-        ],
-        tags: ["React", "Node.js"],
-        demoLink: "https://demo-link.com",
-        repoLink: "https://github.com/repo",
-      },
+
 
       {
         id: 6,
-        title: "Project One",
-        description: "A brief description of the project.",
-        images: [
-          "/pr1.3.png",
-          "/pr1.2.png",
-          "/pr1.3.png"
-        ],
-        tags: ["React", "Node.js"],
-        demoLink: "https://demo-link.com",
-        repoLink: "https://github.com/repo",
+        title: "ToDo App",
+        description: "A task management app built with Next.js and Zod, offering client-side validation and persistent local storage to keep tasks saved even after a page refresh",
+          tags: ["React", "Node.js", "ZOD"],
+        demoLink: "https://todo-zod.vercel.app/",
+        repoLink: "https://github.com/menoo-o/todo_ZOD.git",
       },
     
-    
-    
-
-      
     ]
 
 export default function Projects() {
